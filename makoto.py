6877478401704aa5c1d34eddebff746c3c3b8108
PKWBESTCOF <pierreksar1.0@gmail.com>
2022-06-27 11:42:18 +0200

    Initial commit

---
+++ makoto.py
@@ -0,0 +1,24 @@
+import discord
+from discord.ext import commands
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv('.env')
+load_dotenv(verbose=True)
+TOKEN = os.getenv('TOKEN')
+
+intents=intents=discord.Intents.all()
+bot = commands.Bot(command_prefix ="!", description ="Makoto", intents=intents)
+
+@bot.event
+async def on_ready():
+    print("MAKOTO started successfully !")
+
+@bot.event
+async def on_message(message):
+    if bot.user.mentioned_in(message):
+        chan = bot.get_channel(990909559906377729)
+        await message.channel.send(f"Hello ! Je suis plein developpement, certaines fonctionnalités ne sont donc pas disponible, regarde le channel {chan.mention} pour suivre mon developpement !")
+
+bot.run(TOKEN)
