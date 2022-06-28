2a15e2bec0cb1cc50a1ee32a3a27b141c6a8f53b
PKWBESTCOF <pierreksar1.0@gmail.com>
2022-06-28 10:54:42 +0200

    mention fix

--- makoto.py
+++ makoto.py
@@ -26,11 +26,13 @@ async def on_ready():
 @bot.event
 async def on_message(message):
     print(message.content)
-    """
     if bot.user.mentioned_in(message):
-        chan = bot.get_channel(990909559906377729)
-        await message.channel.send(f"Hello ! Je suis plein developpement, certaines fonctionnalités ne sont donc pas disponible, regarde le channel {chan.mention} pour suivre mon developpement !")
-    """
+        if message.content in ("@here","@everyone")
+            print("dégage")
+        else:
+            chan = bot.get_channel(990909559906377729)
+            await message.channel.send(f"Hello ! Je suis plein developpement, certaines fonctionnalités ne sont donc pas disponible, regarde le channel {chan.mention} pour suivre mon developpement !")
+
 
 bot.add_cog(cogs.join.member_join(bot))
 bot.run(TOKEN)
