4353dc888d004f3e3b68d3450f593bb8c2126ad1
PKWBESTCOF <pierreksar1.0@gmail.com>
2022-06-28 10:47:38 +0200

    add import in main

--- makoto.py
+++ makoto.py
@@ -25,10 +25,12 @@ async def on_ready():
 
 @bot.event
 async def on_message(message):
+    print(message)
+    """
     if bot.user.mentioned_in(message):
         chan = bot.get_channel(990909559906377729)
         await message.channel.send(f"Hello ! Je suis plein developpement, certaines fonctionnalités ne sont donc pas disponible, regarde le channel {chan.mention} pour suivre mon developpement !")
-
+    """
 
 bot.add_cog(cogs.join.member_join(bot))
 bot.run(TOKEN)
