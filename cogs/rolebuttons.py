e0c31da9ee3746fd9265fb6620d3a4e7c6b23ae9
PKWBESTCOF <pierreksar1.0@gmail.com>
2022-06-30 23:08:03 +0200

    fix

--- cogs/rolebuttons.py
+++ cogs/rolebuttons.py
@@ -3,14 +3,29 @@ from discord.ext import commands
 from discord_slash import SlashCommand
 from discord_slash.utils.manage_components import *
 
+adminid = (264335766216376320)
+
+
 class role_button(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    async def cog_check(self, ctx):
+        user = ctx.author
+        identifiant = user.id
+        return identifiant in adminid
+
     @commands.command()
     async def createrolebutton(self,ctx):
         embed = discord.Embed(
-            title="Ci-dessous un menu déroulant vous permettant de faire un ticket dans la catégorie de votre choix.",
-            color=0xB90000)
+            title="Hello, veuilliez choisir les jeux auquelles vous jouez ! 💜​",
+            color=0xAD0DE4)
         embed.set_footer(text=self.bot.user.name, icon_url=self.bot.user.avatar_url)
         await ctx.send(embed=embed)
+
+    @commands.command()
+    async def addrole(self, ctx, role : discord.Role, e):
+        embed = discord.Embed(
+            title="Pour choisir le  ",
+            color=0xAD0DE4)
+        embed.set_footer(text=self.bot.user.name, icon_url=self.bot.user.avatar_url)
