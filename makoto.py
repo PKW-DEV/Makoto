a3009be17d7291655ae86299da708052c5f19b2f
PKWBESTCOF <pierreksar1.0@gmail.com>
2022-06-30 10:31:29 +0200

    fix cmd

--- makoto.py
+++ makoto.py
@@ -19,6 +19,11 @@ slash = SlashCommand(bot, sync_commands=True)
 
 @bot.event
 async def on_ready():
+    guild = ctx.guild
+    r = discord.utils.get(guild.roles, name="Non vérifié")
+    for chan in guild.channels:
+        await chan.set_permissions(r, send_messages=False, read_messages=False)
+        print(f"{c} add non verifié read_message in false")
     c = bot.get_channel(990927066377617439)
     await c.send("```MAKOTO STARTED SUCCESSFULLY```")
     print("MAKOTO started successfully !")
@@ -30,15 +35,9 @@ async def on_message(message):
         if message.content in ("@here","@everyone"):
             return
         else:
+            bot.get_channel(990909559906377729)
             await message.channel.send(f"Hello ! Je suis plein developpement, certaines fonctionnalités ne sont donc pas disponible, regarde le channel {chan.mention} pour suivre mon developpement !")
 
-@bot.command()
-async def locknv(ctx):
-    guild = ctx.guild
-    r = discord.utils.get(guild.roles, name="Non vérifié")
-    for chan in guild.channels:
-        await chan.set_permissions(r, read_message=False)
-        print(f"{c} add non verifié read_message in false")
 
 bot.add_cog(cogs.join.member_join(bot))
 bot.run(TOKEN)
