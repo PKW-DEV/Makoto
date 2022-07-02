0565a37ebfaa88a6873c008bb4be6a779ae28f20
PKWBESTCOF <pierreksar1.0@gmail.com>
2022-07-02 13:50:29 +0200

    new !help

--- makoto.py
+++ makoto.py
@@ -34,6 +34,22 @@ async def on_message(message):
             bot.get_channel(990909559906377729)
             await message.channel.send(f"Hello ! Je suis plein developpement, certaines fonctionnalités ne sont donc pas disponible, regarde le channel {chan.mention} pour suivre mon developpement !")
 
+class MyHelp(commands.HelpCommand):
+    async def send_bot_help(self, mapping):
+        embed = discord.Embed(title="Aide aux commandes Makoto.", color=0xAD0DE4)
+        embed.set_footer(text=self.bot.user.name, icon_url=self.bot.user.avatar_url)
+        for cog, commands in mapping.items():
+           command_signatures = [self.get_command_signature(c) for c in commands]
+           if command_signatures:
+                cog_name = getattr(cog, "qualified_name", "No Category")
+                embed.add_field(name=cog_name, value="\n".join(command_signatures), inline=False)
+
+        channel = self.get_destination()
+        await channel.send(embed=embed)
+
+bot.help_command = MyHelp()
+
+
 bot.add_cog(cogs.rolebuttons.role_button(bot))
 bot.add_cog(cogs.join.member_join(bot))
 bot.run(TOKEN)
